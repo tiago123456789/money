38bd097631dbd221643a6a5a1dfb8d09df81db47
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-04-22 18:25:42 -0300

    implementing findAll, findById and save methods in class LancamentoService

---
+++ src/main/java/com/tiago/money/money/repository/LancamentoRepository.java
@@ -0,0 +1,6 @@
+package com.tiago.money.money.repository;
+
+import com.tiago.money.money.model.Lancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {}
