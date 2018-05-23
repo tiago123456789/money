61ac0984513f5841e601df30472e83c52a302830
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-05-23 12:36:35 -0300

    Added classe Authorization Server

---
+++ src/main/java/com/tiago/money/money/config/AuthorizationServerSecurity.java
@@ -0,0 +1,33 @@
+package com.tiago.money.money.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerSecurity extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("angular")
+                .secret("@ngul@r0")
+                .scopes("read", "write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(1800);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+}
